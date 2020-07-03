84934e630f6337a215a095bcf43a10e2f3d68fe1
Dmytro Budnyk <dmitriy.budnik@gmail.com>
2020-07-03 11:38:55 +0300

    Adds map

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+
+import GoogleMapReact from 'google-map-react';
+
+const isClient = typeof window !== 'undefined';
 // import styled from '@emotion/styled';
 // import { css } from '@emotion/core';
 import {
@@ -8,12 +12,31 @@ import {
 
 import { Layout } from '~/components/Layout';
 import { SEO } from '~/components/SEO';
+const API_KEY = 'AIzaSyDHuRkzMtkC6tu8Mxvp8xmz7iR9QqkBSvk';
 
 const Contact: React.FC<{ intl: IntlShape }> = ({ intl: { formatMessage } }) => {
   return (
     <Layout>
       <SEO title={formatMessage({ id: 'contact.title' })} />
       <h1>{formatMessage({ id: 'contact.title' })}</h1>
+
+      <section className="google-map">
+      <div className="map">
+        { isClient && (
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: API_KEY }}
+            defaultCenter={[1, 1]}
+            defaultZoom={1}
+          >
+            <div
+              className="marker"
+              lat={1}
+              lng={1}
+            />
+          </GoogleMapReact>
+        )}
+      </div>
+    </section>
     </Layout>
   );
 };
