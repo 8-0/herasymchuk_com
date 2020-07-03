942eeeeb76a87d09cf753b56dbf6e56255519b08
Dmytro Budnyk <dmitriy.budnik@gmail.com>
2020-07-03 13:19:39 +0300

    Adds google maps

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import GoogleMapReact from 'google-map-react';
+import { ReactComponent as Marker } from '~/icons/marker.svg';
+
 
 const isClient = typeof window !== 'undefined';
 // import styled from '@emotion/styled';
 // import { css } from '@emotion/core';
 import {
+  IntlContextConsumer,
   injectIntl,
   IntlShape
 } from 'gatsby-plugin-intl';
@@ -19,24 +22,22 @@ const Contact: React.FC<{ intl: IntlShape }> = ({ intl: { formatMessage } }) => 
     <Layout>
       <SEO title={formatMessage({ id: 'contact.title' })} />
       <h1>{formatMessage({ id: 'contact.title' })}</h1>
-
-      <section className="google-map">
-      <div className="map">
-        { isClient && (
+      {isClient && (
+        <div style={{ height: '100vh', width: '100%' }}>
           <GoogleMapReact
             bootstrapURLKeys={{ key: API_KEY }}
-            defaultCenter={[1, 1]}
-            defaultZoom={1}
+            defaultCenter={[48.4691, 35.0235]}
+            defaultZoom={15}
           >
-            <div
-              className="marker"
-              lat={1}
-              lng={1}
-            />
+            <div style={{position: 'absolute', transform: 'translate(-50%, -50%)'}}>
+            <Marker/>
+            <IntlContextConsumer>
+              {({ language }) => <img src={`/logo_${language}.png`} alt='HSS logo' style={{ width: '3rem'}}/> }
+            </IntlContextConsumer>
+            </div>
           </GoogleMapReact>
-        )}
-      </div>
-    </section>
+        </div>
+      )}
     </Layout>
   );
 };
