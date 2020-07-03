6560728a9def1f77fde2cc65f65e8eb1dba998b2
Dmytro Budnyk <dmitriy.budnik@gmail.com>
2020-07-03 13:56:54 +0300

    Revert "Leaflet"
    This reverts commit 7b5ffc3bae0f3daeff86001437abc99fa97d3ff1.

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,29 +1,43 @@
 import React from 'react';
 
-import { ReactComponent as Mrker } from '~/icons/marker.svg';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import GoogleMapReact from 'google-map-react';
+import { ReactComponent as Marker } from '~/icons/marker.svg';
+
 
 const isClient = typeof window !== 'undefined';
 // import styled from '@emotion/styled';
 // import { css } from '@emotion/core';
-import { IntlContextConsumer, injectIntl, IntlShape } from 'gatsby-plugin-intl';
-const defaultCenter = [48.4691, 35.0235];
+import {
+  IntlContextConsumer,
+  injectIntl,
+  IntlShape
+} from 'gatsby-plugin-intl';
 
 import { Layout } from '~/components/Layout';
 import { SEO } from '~/components/SEO';
+const API_KEY = 'AIzaSyDHuRkzMtkC6tu8Mxvp8xmz7iR9QqkBSvk';
 
 const Contact: React.FC<{ intl: IntlShape }> = ({ intl: { formatMessage } }) => {
   return (
     <Layout>
       <SEO title={formatMessage({ id: 'contact.title' })} />
       <h1>{formatMessage({ id: 'contact.title' })}</h1>
-        <div style={{  width: '300px', height: '300px'  }}>
-          <Map center={defaultCenter} zoom={14}>
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-          </Map>
+      {isClient && (
+        <div style={{ height: '100vh', width: '100%' }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: API_KEY }}
+            defaultCenter={[48.4691, 35.0235]}
+            defaultZoom={15}
+          >
+            <div style={{position: 'absolute', transform: 'translate(-50%, -50%)'}}>
+            <Marker/>
+            <IntlContextConsumer>
+              {({ language }) => <img src={`/logo_${language}.png`} alt='HSS logo' style={{ width: '3rem'}}/> }
+            </IntlContextConsumer>
+            </div>
+          </GoogleMapReact>
         </div>
+      )}
     </Layout>
   );
 };
